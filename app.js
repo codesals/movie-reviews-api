ea2fcb47afe426cda3f9832da0edb5cb5cf72f08
Sally <78157471+codesals@users.noreply.github.com>
2021-04-04 22:23:26 +0300

    Initialize Express App & Set up .env

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const ip = require("ip");
+const db = require("./db/models");
+
+// importing routers
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const passport = require("passport");
+//const { localStrategy, jwtStrategy } = require("./middleware/passport");
+
+// init app
+const app = express();
+
+// middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(passport.initialize());
+// passport.use(localStrategy);
+// passport.use(jwtStrategy);
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// start server
+const runApp = async () => {
+  try {
+    await db.sequelize.sync({ alter: true });
+
+    app.listen(process.env.PORT, () => {
+      console.log("Express app started succeffully");
+      console.log(`Running on ${ip.address()}:${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Failed to start server:", error);
+  }
+};
+
+runApp();
+
+module.exports = app;
---
+++ readme.md
@@ -0,0 +1,13 @@
+# env variables
+
+When you first clone this repo, add a `.env` file to the root directory.
+
+The variables include the following:
+
+```
+PORT=8000
+DB_USERNAME=postgres
+DB_PASSWORD=fssp21
+DB_NAME=moviereviews_db
+DB_HOST=127.0.0.1
+```
